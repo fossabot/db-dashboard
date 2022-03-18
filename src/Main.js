d198852ed39effbdade76b6798e5639c1079f533
ttheolier <theolier@usc.edu>
2022-03-18 10:55:07 -0700

    more deletes

--- src/Main.js
+++ src/Main.js
@@ -3,10 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import CreatePool from "./pages/CreatePool";
 import Home from "./pages/Home";
-import PoolList from "./pages/PoolList";
-import SchemaList from "./pages/SchemaList";
-import TableList from "./pages/TableList";
-import TableView from "./pages/TableView";
 
 function Main() {
   return (
@@ -15,14 +11,6 @@ function Main() {
       <Route exact path="/" element={<SignIn />} />
       <Route exact path="/createpool" element={<CreatePool />} />
       <Route exact path="/home" element={<Home />} />
-      <Route exact path="/pools" element={<PoolList />} />
-      <Route exact path="/:moatName" element={<SchemaList />} />
-      <Route exact path="/:moatName/:schemaName" element={<TableList />} />
-      <Route
-        exact
-        path="/:moatName/:schemaName/:tableName"
-        element={<TableView />}
-      />
 
       {/*<Route element={<SignIn/>} />*/}
     </Routes>
